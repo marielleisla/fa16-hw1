cdb37f2bdb3b8ed997f31d239c9a2954c46768bd
Marielle Isla <r.m.a.isla@berkeley.edu>
2016-09-21 11:47:13 -0700

    Finished HW1

--- foobar.rb
+++ foobar.rb
@@ -3,6 +3,12 @@ class Foobar
   def self.baz(a)
     # Class method
     # Call with `Foobar.baz`
+    a.map! { |x| x.to_i + 2 }
+    a.delete_if { |x| x % 2 != 0 || x > 10}
+    a.uniq!
+    sum = 0
+    a.each { |x| sum = sum + x}
+    sum
   end
 end
 
--- hw1.rb
+++ hw1.rb
@@ -1,16 +1,17 @@
 def squared_sum(a, b)
   # Q1 CODE HERE
-
+  (a + b) * (a + b)
 end
 
 def sort_array_plus_one(a)
   # Q2 CODE HERE
-
+   a.map! { |x| x + 1 }
+   a.sort
 end
 
 def combine_name(first_name, last_name)
   # Q3 CODE HERE
-
+  name =  "#{first_name} #{last_name}"
 end
 
 def blockin_time(a)
